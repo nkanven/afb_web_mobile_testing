8626f60bd226be7c0af91ee1c78d2b8365d3fd43
Nkondog A. Venceslas <nkanven@gmail.com>
2023-10-29 13:10:27 +0100

    Modify udid

--- src/test/java/org/example/android/SaraTest.java
+++ src/test/java/org/example/android/SaraTest.java
@@ -25,12 +25,13 @@ public class SaraTest {
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setCapability("deviceName", "sdk_gphone64_x86_64");
         cap.setCapability("udid", "emulator-5554");
+        //cap.setCapability("udid", "18271FDF60018Y");
         cap.setCapability("platformName", "Android");
         cap.setCapability("platformVersion", "13");
         cap.setCapability("appium:automationName", AutomationName.ANDROID_UIAUTOMATOR2);
         cap.setCapability("app", "C:/Users/CM-Nkapro/IdeaProjects/untitled1/src/test/java/org/example/android/SARA_BY_AFRILAND_CAMEROON_2.2.6_apkcombo.com.apk");
 
-        driver = new AppiumDriver(new URL("http://127.0.0.1:4723"), cap);
+        driver = new AppiumDriver(new URL("http://0.0.0.0:4723"), cap);
 
         System.out.println("Application started...");
     }
